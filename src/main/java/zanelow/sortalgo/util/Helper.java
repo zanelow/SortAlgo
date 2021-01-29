cabb666e145a1ed0959270aee410068a057d763b
zanelow <mintakaz@hotmail.com>
2021-01-30 07:54:20 +0800

    Removing code smells

--- src/main/java/zanelow/sortalgo/util/Helper.java
+++ src/main/java/zanelow/sortalgo/util/Helper.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public final class Helper {
 
+    private static final Random random = new Random();
+
     private Helper() {}
 
     public static Integer[] createRandomArray(final int size) {
         Integer[] array = new Integer[size];
-        Random random = new Random();
 
         for(int i=0; i<array.length; i++) {
             // we don't need a big size to play around
